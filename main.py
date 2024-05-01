74b457c30373d219b122a633da2f474aa5841314
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-01 21:57:56 +0700

    Python isdisjoint() method examples

--- main.py
+++ main.py
@@ -4,3 +4,20 @@ if __name__ == "__main__":
   """
   set_a.isdisjoint(set_b)
   """
+
+  # Python isdisjoint() method examples
+
+  odd_numbers = {1, 3, 5}
+  even_numbers = {2, 4, 6}
+
+  print(odd_numbers)
+  print(even_numbers)
+  print(odd_numbers.isdisjoint(even_numbers))
+
+  letters = {"A", "B", "C"}
+  alphanumerics = {"A", 1, 2}
+
+  print(letters)
+  print(alphanumerics)
+  print(letters.isdisjoint(alphanumerics))
+  print(letters.isdisjoint(["A", 1, 2]))
